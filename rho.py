fe75aa14b62cd3968a2aa037e187c0fbf0f7e939
alanburzevski <alan.burzevski@gmail.com>
2021-07-11 01:37:00 +1000

    adding log changes, debugging, and varcov and weights

--- rho.py
+++ rho.py
@@ -56,25 +56,26 @@ def readTraining ():
 
 # Given the price history, output daily percentage price change matrix
 # Input: givenPrices (price history)
-# Output: matrix of daily percentage change; dailyPChange[i-1][j-1] = percentage change of instrument j between days i and i-1
-def dailyPChange (givenPrices):
-    pChange = []
+# Output: matrix of daily percentage change; dailyReturns[i-1][j-1] = percentage change of instrument j between days i and i-1
+def dailyReturns (givenPrices):
+    logChange = []
     # loop through each instrument
     for inst in givenPrices.T:
-        pChangeInst = np.zeros(nDays)
-        for day in range(1, nDays):
-            pChangeInst[day] = np.log(inst[day] / inst[day - 1])
-        pChange.append(pChangeInst)
-    pChange = np.array(pChange)
-    # print(pChange.T.shape)
-    return pChange.T
+        logChangeInst = np.empty((nDays, 1))
+        # logChangeInst = np.zeros(nDays)
+        for day in range(0, nDays - 1):
+            logChangeInst[day] = np.log(inst[day + 1] / inst[day])
+        logChange.append(logChangeInst)
+    logChange = np.array(logChange)
+    # print(logChange.T.shape)
+    return logChange.T
 
 # Given the daily price changes, output the avg. daily return, SD, variance for each instrument
-def returnMeasures (pChange):
-    measures = np.zeros((3, nInst))
-    measures[0] = [np.average(inst) for inst in pChange.T]
-    measures[1] = [np.std(inst, ddof=1) for inst in pChange.T]
-    measures[2] = [np.var(inst, ddof=1) for inst in pChange.T]
+def returnMeasures (dailyReturns):
+    measures = np.empty((3, nInst))
+    measures[0] = [np.average(inst) for inst in dailyReturns.T]
+    measures[1] = [np.std(inst, ddof=1) for inst in dailyReturns.T]
+    measures[2] = [np.var(inst, ddof=1) for inst in dailyReturns.T]
     return measures
 
 # Given the price history, output the excess returns matrix
@@ -83,19 +84,52 @@ def returnMeasures (pChange):
 def excessReturns (givenPrices):
     excessReturns = []
     # get the required matrices for further computation
-    pChange = dailyPChange(givenPrices)
-    measures = returnMeasures(pChange)
+    Returns = dailyReturns(givenPrices)
+    measures = returnMeasures(Returns)
 
     for inst in range(nInst):
-        instExcess = np.zeros(nDays) 
-        instPChange = (pChange.T)[inst]
-        for i in range (1, nDays):
-            instExcess[i] = instPChange[i] - measures[0][inst]
+        instExcess = np.empty(nDays - 1) 
+        instReturns = (Returns.T)[inst]
+        for i in range (0, nDays - 1):
+            instExcess[i] = instReturns[i] - measures[0][inst]
         excessReturns.append(instExcess)
 
     excessReturns = np.array(excessReturns)
     print(excessReturns.T.shape)
     return excessReturns.T
+
+# Calculate the variance covariance matrix
+# Input: excess returns
+# Output: variance covariance matrix
+def varCov (givenExcess):
+    varCovMat = np.matmul(givenExcess.T, givenExcess)/(249 - 1) # -1 because sample std
+    return varCovMat
+
+# Calculate the scaled variance covariance matrix
+# Input: variance covariance matrix
+# Output: scaled variance covariance matrix
+def sigma (varCovMat):
+    sigmaMat = varCovMat*250
+    return sigmaMat
+
+# Calculate weights
+# Input: 
+# Output: weights
+def getWeights(returns, sigma, targetReturn):
+    ones = np.ones(nInst)
+    A = np.matmul(np.matmul(ones, np.linalg.inv(sigma)), ones.T)
+    B = np.matmul(np.matmul(ones, np.linalg.inv(sigma)), returns.T)
+    C = np.matmul(np.matmul(returns, np.linalg.inv(sigma)), returns.T)
+    delta = A * C - B**2
+    lam = (C - targetReturn*B)/delta
+    gam = (targetReturn*A - B)/delta
+
+    weightsTerm1 = lam * np.matmul(np.linalg.inv(sigma), ones.T)
+    weightsTerm2 = gam * np.matmul(np.linalg.inv(sigma), returns.T)
+    weights = weightsTerm1 + weightsTerm2
+    return(weights)
+
+excessReturns(readTraining())
 
 # TEST CODE
 ##########################################################################
